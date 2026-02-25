299d278e983b89e84d6ddee61091e488a023403d
Arthur_HM <arthurmenegat64@gmail.com>
2026-02-25 11:26:16 -0300

    Primeiro commit

--- MediaMovelEx.c
+++ MediaMovelEx.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+static void media_movel(unsigned int janela);
+
+static void media_movel(unsigned int janela)
+{
+    printf("%u", janela);
+}
+
 int main(void)
 {
-    puts("Hello, World!");
+    unsigned int janela = 0;
+
+    puts("Insira a janela de amostragem:");
+    scanf("%u", &janela);
+
+    media_movel(janela);
 
     return 0;
 }
