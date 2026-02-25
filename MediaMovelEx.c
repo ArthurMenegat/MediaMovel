aabda351d5376b38cd93f3dd8eb90c29e0ce4830
Arthur_HM <arthurmenegat64@gmail.com>
2026-02-25 13:15:00 -0300

    Segundo commit

--- MediaMovelEx.c
+++ MediaMovelEx.c
@@ -5,18 +5,23 @@ static void media_movel(unsigned int janela);
 
 static void media_movel(unsigned int janela)
 {
-    printf("%u", janela);
+    printf("%u\n", janela);
 }
+
+//change
 
 int main(void)
 {
     unsigned int janela = 0;
     int *ptr = malloc(sizeof(janela));
+    int exit = 0;
 
     puts("Insira a janela de amostragem:");
     scanf("%u", &janela);
 
     media_movel(janela);
+
+    scanf(" %d", &exit);
 
     return 0;
 }
